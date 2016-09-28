fc94e71bbcb5d4056448c91ccf5be4dd7c67b27a
Maddie Cousens <mscousens@gmail.com>
2016-09-28 07:34:53 -0700

    Consolidate into one function, passing files as list

--- produce_summary.py
+++ produce_summary.py
@@ -1,49 +1,21 @@
-# This program opens log files for each day, and prints the count, type of melon
-# and total amount sold in $
-#
-# It could definitely be broken into functions
+# This program takes a list of log files and uses the function
+# delivery_report to print the number of melons delivery, the type of melon
+# delivered, and the total amount of money ($) made from the delivery.
 
-
-print "Day 1"
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print "Delivered {} {}s for total of ${}".format(
-        count, melon, amount)
-the_file.close()
-
-
-print "Day 2"
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print "Delivered {} {}s for total of ${}".format(
-        count, melon, amount)
-the_file.close()
-
-
-print "Day 3"
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print "Delivered {} {}s for total of ${}".format(
-        count, melon, amount)
-the_file.close()
+def delivery_report(files):
+    """print delivery report of cost, amount sold, type"""
+    # Iterate through files
+    for file in files:
+        the_file = open(file)
+        # Iterate through lines in file, breaking lines by '|' into list of
+        # strings
+        for line in the_file:
+            line = line.rstrip()
+            words = line.split('|')
+            print "Delivered {} {}s for total of ${}".format(words[1], words[0],
+                                                             words[2])
+        the_file.close()
+# List files to be used and call function deliery_report
+files = ["um-deliveries-20140519.txt", "um-deliveries-20140520.txt", 
+         "um-deliveries-20140521.txt"]
+delivery_report(files)
